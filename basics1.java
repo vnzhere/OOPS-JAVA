9da55903e14ef660817f95d2684113a719e403f4
Vanzhere <vaniya.sharma158@gmail.com>
2026-03-22 10:16:46 +0000

    Initial commit

---
+++ basics1.java
@@ -0,0 +1,36 @@
+public class main
+    public static void main(string[]args){
+
+        // store 5 roll numbers
+        int[]no = new int [5];
+
+        // store 5 names
+        string[]names= newstring[5];
+
+        // Data of 5 students (roll no, names, marks)
+        int[] rno = new int[5];
+        string name= new string[5];
+        float[] marks = new float[5];
+
+         
+    }
+    // to shift entire data into a single entity---
+    // we use classes-- which is group of property and function
+    
+   public class main
+    public static void main(string[]args){
+        student[] students= newstudent[5];
+    }
+    student kiran;
+    // creating a class1
+    class student {
+          int[] rno = new int[5];
+        string name= new string[5];
+        float[] marks = new float[5];
+
+    }
+
+    //class2
+    class student
+
+   } 
